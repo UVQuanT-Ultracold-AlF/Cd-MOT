22811cad09c04868e87e50f9cbfc4570fe090b2c
Lajos Palanki <lp618@ic.ac.uk>
2023-06-22 13:03:16 +0100

    Modified script for new output format

--- HPC_scripts/slower_MC.sh
+++ HPC_scripts/slower_MC.sh
@@ -13,4 +13,4 @@ cp -r $HOME/Cd_MOT_paper/Cd-MOT/* $TMPDIR
 cd $TMPDIR
 
 $HOME/anaconda3/bin/python slower_scan_MC.py 10000 256
-cp $TMPDIR/out.npy $HOME/Cd_MOT_paper/Cd-MOT/out.npy
+cp $TMPDIR/out.npz $HOME/Cd_MOT_paper/Cd-MOT/out.npz
